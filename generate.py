e8a69d9fd59debaa662f3223158c42fe5b23f104
jackv12 <jvickers@uvm.edu>
2025-01-27 23:16:56 -0500

    Finished assignment 2

--- generate.py
+++ generate.py
@@ -1,2 +1,11 @@
 #Jack Vickers
+import pyrosim.pyrosim as pyrosim
+pyrosim.Start_SDF("box.sdf")
 
+for xAxis in range(0,4):
+    for yAxis in range(0,4):
+        y=1
+        for x in range(1, 11):
+            pyrosim.Send_Cube(name="Box", pos=[xAxis, yAxis, x], size=[y, y, y])
+            y *= .9
+pyrosim.End()
